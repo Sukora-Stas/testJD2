653deab7d1c5ed8306db01512bd352f985983fce
SergeyProkofiev <yomans@yandex.ru>
2017-03-20 08:57:08 +0300

    new branch tasc_calc

--- src/main/java/com/it/academy/App.java
+++ src/main/java/com/it/academy/App.java
@@ -1,9 +1,6 @@
 package com.it.academy;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
---
+++ src/main/java/com/it/academy/Calculation.java
@@ -0,0 +1,5 @@
+package com.it.academy;
+
+public class Calculation {
+
+}
