d7f2c5dbba7081ad123cb2abb48005f54d8b9886
Sukora-Stas <stasiksukora@mail.ru>
2017-03-20 09:14:25 +0300

    create substraction

--- src/main/java/com/it/academy/Calculation.java
+++ src/main/java/com/it/academy/Calculation.java
@@ -2,14 +2,18 @@ package com.it.academy;
 
 public class Calculation {
 
-    public void addition(int first, int second){
+    public void add(int first, int second) {
 
-        if(){
+    }
 
+    public int substraction(Integer a, Integer b) {
+        if (a != null || b != null) {
+            return (a - b);
+        } else {
+            System.out.println("Checkout you numbers");
+            return 0;
         }
+
     }
 
-    public int multiply(int x, int y) {
-        return x * y;
-    }
 }
