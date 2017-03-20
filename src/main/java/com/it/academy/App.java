6437ed5a84bed35162181ad2ea389507e96c79fa
artur urban <yanapotme4@gmail.com>
2017-03-20 08:59:50 +0300

    added multiply method

--- src/main/java/com/it/academy/App.java
+++ src/main/java/com/it/academy/App.java
@@ -2,12 +2,14 @@ package com.it.academy;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
 
     }
+
+    public int multiply(int x, int y) {
+        return x * y;
+    }
+
 }
