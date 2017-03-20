6ad8ffe4ec6ad3a009bff457338e2ed7f2e32003
Anton Salva <antonsalva@icloud.com>
2017-03-20 09:11:40 +0300

    Dev operation

--- src/main/java/com/it/academy/Calculation.java
+++ src/main/java/com/it/academy/Calculation.java
@@ -1,8 +1,18 @@
 package com.it.academy;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 public class Calculation {
 
     public void add(int first, int second){
 
+    }
+
+    public double dev(double first, double second) {
+        try {
+            return first / second;
+        } catch (ArithmeticException arEx) {
+            System.out.println("Недопустимая операция");
+        }
     }
 }
