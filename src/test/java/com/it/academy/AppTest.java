5183741c8c265f8643a71785ea52fe0824270f9a
SergeyProkofiev <yomans@yandex.ru>
2017-03-20 09:35:20 +0300

    new method calculation 2.0 + tests 1.0 и пробел

--- src/test/java/com/it/academy/AppTest.java
+++ src/test/java/com/it/academy/AppTest.java
@@ -3,19 +3,20 @@ package com.it.academy;
 import junit.framework.TestCase;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Unit test for simple App.
  */
-public class AppTest
-        extends TestCase {
+public class AppTest{
 
     @Test
-    public void additionTest(){
+    public void additionTest() {
 
         Calculation calc = new Calculation();
 
-        assertEquals("must be 10",10,calc.addition(5,5));
-
+        assertEquals("must be 10", 10, calc.addition(5, 5));
 
     }
+
 }
